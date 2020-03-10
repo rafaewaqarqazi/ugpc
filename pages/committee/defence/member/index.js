e485de30e69ea879107bbd061eb440ca7c583980
rafaewaqarqazi <rafaewaqar@gmail.com>
2020-03-10 15:09:24 +0500

    Fixed a bug (Committee Member Could not Comment)

--- pages/committee/defence/member/index.js
+++ pages/committee/defence/member/index.js
@@ -1,12 +1,15 @@
 import CommitteeMemberLayout from "../../../../components/Layouts/CommitteeMemberLayout";
 import DefenceMeetingComponent from "../../../../components/coordinator/DefenceMeetingComponent";
 import {withDefenceMemberAuthSync} from "../../../../components/routers/defenceMemberAuth";
+import VisionDocsState from "../../../../context/visionDocs/VisionDocsState";
 
 const Index = () => {
   return (
-    <CommitteeMemberLayout committeeType='Defence' position='Member'>
-      <DefenceMeetingComponent/>
-    </CommitteeMemberLayout>
+    <VisionDocsState>
+      <CommitteeMemberLayout committeeType='Defence' position='Member'>
+        <DefenceMeetingComponent/>
+      </CommitteeMemberLayout>
+    </VisionDocsState>
   );
 };
 
