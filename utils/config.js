be7f0c8868900212d08e76077527d74b8e081c61
rafaewaqarqazi <rafaewaqar@gmail.com>
2023-08-27 13:54:32 +0500

    env config

--- utils/config.js
+++ utils/config.js
@@ -1,2 +1,2 @@
-console.log({ NODE_ENV: process.env });
-export const serverUrl = process.env.SERVER_URL || "http://localhost:5000/api";
+console.log({ NODE_ENV: process.env.SERVER_URL });
+export const serverUrl = process.env.SERVER_URL;
